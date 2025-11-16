9ca289c8bcf507c3dc7bb458170662ff72b3757a
Gabriel_FS13 <falcaoshadowbr@gmail.com>
2025-11-15 22:50:31 -0300

    filtros na pagina de procurar ocorrencias

--- Bot_chat-main/plataforma_web/Frontend/safeproof/src/pages/ProfessionalPage.jsx
+++ Bot_chat-main/plataforma_web/Frontend/safeproof/src/pages/ProfessionalPage.jsx
@@ -3,8 +3,7 @@ import './ProfessionalPage.css'
 export default function ProfessionalPage({ userID }) {
     const API_URL = process.env.REACT_APP_API_URL;
     const [ocorrencias, setOcorrencias] = useState([])
-
-    console.log(API_URL)
+    const [ocorrenciasOriginal, setOcorrenciasOriginal] = useState([])
     useEffect(() => {
         const fetchOcorrencias = async () => {
 
@@ -13,14 +12,10 @@ export default function ProfessionalPage({ userID }) {
                 const getOcorrencias = await fetch(`${API_URL}/get_home`, {
                     method: "GET"
                 })
-
                 const ocorrencias = await getOcorrencias.json()
 
-                console.log(ocorrencias)
-
-                setOcorrencias(ocorrencias.ocorrencias)
-
-
+                setOcorrencias(ocorrencias.ocorrencias.filter(({ ocorrencia }) => ocorrencia.id_responsavel != userID))
+                setOcorrenciasOriginal(ocorrencias.ocorrencias.filter(({ ocorrencia }) => ocorrencia.id_responsavel != userID))
             } catch (err) {
                 console.error(err)
             } finally {
@@ -58,52 +53,160 @@ export default function ProfessionalPage({ userID }) {
         fetchOcorrencia()
     }
 
+    const formatDate = (dateString) => {
+        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+        return new Date(dateString).toLocaleDateString('pt-BR', options);
+    };
+
+
     const listaCrimes = [
+        { id_crime: 5, nome_crime: "Selecione o tipo do crime" },
         { id_crime: 8, nome_crime: 'AMEACA' },
         { id_crime: 13, nome_crime: 'DESCONHECIDO' },
         { id_crime: 11, nome_crime: 'ESTELIONATO' },
         { id_crime: 9, nome_crime: 'HONRA' },
-        { id_crime: 10, nome_crime: 'PERSEGUICAO' },
+        { id_crime: 10, nome_crime: 'PERSEGUIÇÃO' },
         { id_crime: 6, nome_crime: 'RACISMO' },
-        { id_crime: 12, nome_crime: 'VIOLENCIA_DOMESTICA' },
-        { id_crime: 5, nome_crime: "Selecione o tipo do crime" }
+        { id_crime: 12, nome_crime: 'VIOLENCIA DOMESTICA' }
     ]
 
+    const handleDateFilter = (e) => {
+        if (e.target.value === "recentes") {
+            setOcorrencias(
+                [...ocorrencias.sort((a, b) =>
+                    new Date(b.ocorrencia.created_at) - new Date(a.ocorrencia.created_at)
+                )]
+            );
+
+        } else {
+            setOcorrencias(
+                [...ocorrencias.sort((a, b) =>
+                    new Date(a.ocorrencia.created_at) - new Date(b.ocorrencia.created_at)
+                )]
+            );
+
+        }
+    }
+
+    const handleGravidadeFilter = (e) => {
+
+        const ordem = {
+            BAIXA: 1,
+            MEDIA: 2,
+            ALTA: 3
+        }
+
+        if (e.target.value === "baixa") {
+            setOcorrencias(
+                [...ocorrencias.sort((a, b) =>
+                    ordem[a.ocorrencia.gravidade] - ordem[b.ocorrencia.gravidade]
+                )]
+            );
+        } else {
+            setOcorrencias(
+                [...ocorrencias.sort((a, b) =>
+                    ordem[b.ocorrencia.gravidade] - ordem[a.ocorrencia.gravidade]
+                )]
+            );
+        }
+    }
+
+
+    const handleTipoCrime = (e) => {
+
+        const tipo_crime = e.target.value
+        const { id_crime } = listaCrimes.find(crime => {
+            if (crime.nome_crime == tipo_crime) {
+                return crime.id_crime
+            }
+        })
+
+        if (tipo_crime !== "Selecione o tipo do crime") {
+            setOcorrencias(ocorrenciasOriginal.filter(({ ocorrencia }) => ocorrencia.id_crime == id_crime))
+
+            
+        } else {
+            setOcorrencias(ocorrenciasOriginal)
+        }
+    }
+
+
     return (
-        <div className="container_ocorrencias">
-            <section>
-                MENU PARA FILTRAR OCORRENCIAS
+        <div className="ocorrencias_page">
+            <section className="ocorrencias_filtros">
+                <menu>
+                    <ul>
+                        <li>
+                            <select onChange={handleDateFilter}>
+                                <option value="recentes">
+                                    Mais Recentes
+                                </option>
+                                <option value="antigas">
+                                    Mais antigas
+                                </option>
+                            </select>
+                        </li>
+                        <li>
+                            <select onChange={handleGravidadeFilter}>
+                                <option value="baixa">
+                                    Gravidade baixa > Gravidade Alta
+                                </option>
+                                <option value="alta">
+                                    Gravidade alta > Gravidade baixa
+                                </option>
+                            </select>
+                        </li>
+                        <li>
+                            <select onChange={handleTipoCrime}>
+                                {listaCrimes.map(crime => (
+                                    <option key={crime.id_crime} value={crime.nome_crime}>
+                                        {crime.nome_crime}
+                                    </option>
+                                ))}
+                            </select>
+                        </li>
+                    </ul>
+                </menu>
             </section>
-            {ocorrencias.map((ocorrencia, i) => (
-                <div key={ocorrencia.ocorrencia.id_ocorrencia} className="ocorrencias_card">
-                    <div className="ocorrencias_header">
-                        <span>ID da ocorrência:{ocorrencia.ocorrencia.id_ocorrencia}</span>
-                        <span className={`ocorrencia_${ocorrencia.ocorrencia.gravidade}`}>{ocorrencia.ocorrencia.gravidade}</span>
-                        <span>
-                            {ocorrencia.ocorrencia.updated_at}
-                        </span>
-                    </div>
+            <section className="container_ocorrencias">
+                {ocorrencias.length > 0 ? ocorrencias?.map((ocorrencia, i) => (
+                    <div key={ocorrencia.ocorrencia.id_ocorrencia} className="ocorrencias_card">
+                        <div className="ocorrencias_header">
+                            <span>ID da ocorrência:{ocorrencia.ocorrencia.id_ocorrencia}</span>
+                            <span className={`ocorrencia_${ocorrencia.ocorrencia.gravidade}`}>{ocorrencia.ocorrencia.gravidade}</span>
+                            <span>
+                                {formatDate(ocorrencia.ocorrencia.created_at)}
+                            </span>
+                        </div>
+                        <div className="ocorrencias_infos">
+                            <span>
+                                Tipo do crime: {listaCrimes.find(crime => {
+                                    if (crime.id_crime == ocorrencia.ocorrencia.id_crime) {
+                                        return (crime.nome_crime)
+                                    }
+                                })?.nome_crime}
+                            </span>
+                            <span>
+                                Quantidade de evidências:  
+                                {ocorrencia.evidencias.length}
+                            </span>
+                        </div>
 
-                    <div className="ocorrencias_infos">
-                        <span>
-                            Tipo do crime: 
-                        </span>
-                        <span>
-                            Quantidade de evidências:
-                            {ocorrencia.evidencias.length}
-                        </span>
+                        <div className="contato_infos">
+                            <span>{ocorrencia.ocorrencia.nome_usuario}</span>
+                            <span> {ocorrencia.ocorrencia.email_usuario}</span>
+                            <span>{ocorrencia.ocorrencia.contato_usuario}</span>
+                        </div>
+                        <div className="container_button_accept">
+                            <button onClick={() => aceitarOcorrencia(ocorrencia.ocorrencia.id_ocorrencia)}>Aceitar ocorrência</button>
+                        </div>
                     </div>
-
-                    <div className="contato_infos">
-                        <span>{ocorrencia.ocorrencia.nome_usuario}</span>
-                        <span> {ocorrencia.ocorrencia.email_usuario}</span>
-                        <span>{ocorrencia.ocorrencia.contato_usuario}</span>
-                    </div>
-                    <div className="container_button_accept">
-                        <button onClick={() => aceitarOcorrencia(ocorrencia.ocorrencia.id_ocorrencia)}>Aceitar ocorrência</button>
-                    </div>
-                </div>
-            ))}
+                )) : (<span style={{
+                    color: 'red'
+                }}>
+                    Nenhuma ocorrência encontrada
+                </span>)}
+            </section>
         </div>
     )
 }
